c79995d8c486d9c4f921ba8c9d0cbab06d671853
nckuwater <F74081195@gs.ncku.edu.tw>
2020-07-31 18:11:33 +0800

    add more craft/target data

--- Assets/Scripts/FlightComputer.cs
+++ Assets/Scripts/FlightComputer.cs
@@ -14,8 +14,9 @@ namespace Assets.Scripts{
         Vector3d craft_r, target_r, craft_v, target_v, craft_unit_r, target_unit_r;
         double craft_v_value, target_v_value,craft_v_r, target_v_r;
         Vector3d craft_h, target_h;
+        double craft_h_value, target_h_value;
         double craft_e, target_e, craft_T, target_T, planet_mu, planet_mass;
-        Vector3d craft_theta, target_theta, craft_p, craft_q;
+        Vector3d craft_theta, target_theta, craft_p, craft_q, target_p, target_q;
         Vector3d craft_E, target_E, craft_Me, target_Me;
 
         string craft_parent_name, target_parent_name; // avoid different PCI transfer.
@@ -56,10 +57,12 @@ namespace Assets.Scripts{
         
             //craft_h = Vector3d.Cross(craft_r, craft_v);// Vector3d
             craft_h = CraftOrbit.AngularMomentum;
+            craft_h_value = CraftOrbit.AngularMomentumMag;
             craft_T = CraftOrbitData.Period;
             craft_e = CraftOrbitData.Eccentricity;
         }
         public void InitializeTargetData(){
+            // r, v, h, e, p, q
             var CraftNode = Game.Instance.FlightScene.CraftNode;
             var CraftScript = CraftNode.CraftScript;
             var CraftFlightData = CraftScript.FlightData;
@@ -69,11 +72,15 @@ namespace Assets.Scripts{
             IOrbit TargetOrbit = TargetOrbitNode.Orbit;
 
             target_r = NavSphereTarget.Position;
+            target_unit_r = Vector3d.Normalize(target_r);
             target_v = NavSphereTarget.Velocity;
+            target_v_value = Vector3d.Magnitude(target_v);
+            target_v_r = Vector3d.Dot(target_v, target_unit_r);
 
             target_h = TargetOrbit.AngularMomentum;
+            target_h_value = TargetOrbit.AngularMomentumMag;
             target_T = TargetOrbitNode.Period;
-            craft_e = CraftOrbitData.Eccentricity;
+            target_e = TargetOrbit.Eccentricity;
 
             Debug.Log(target_r);
             
